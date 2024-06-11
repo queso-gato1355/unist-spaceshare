a64ca5c91dd48cf0f0935a7dcf298d66065d342a
최진우 <jinu2001@unist.ac.kr>
2024-06-12 00:41:28 +0900

    FEAT: Add edit user info

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,6 +1,7 @@
 // src/app/api/user/route.js
 import { connectToDatabase } from "../../../lib/mongodb";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 import { ObjectId } from "mongodb";
 
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -44,6 +45,92 @@ export async function GET(req) {
             },
         });
     } catch (error) {
+        return new Response(JSON.stringify({ user: null }), {
+            status: 403,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
+}
+
+export async function PUT(req) {
+    const authHeader = req.headers.get("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return new Response(JSON.stringify({ user: null }), {
+            status: 401,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
+
+    const formData = await req.formData();
+    const email = formData.get("email");
+    const username = formData.get("username");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+    const contactLink = formData.get("contactLink");
+    const profilePicture = formData.get("profilePicture");
+
+    let profilePicturePath = "";
+    if (profilePicture && profilePicture.size > 0) {
+        const uniqueSuffix = `${Date.now()}-${Math.round(
+            Math.random() * 1e9
+        )}`;
+        const ext = path.extname(profilePicture.name);
+        const filename = `profile-${uniqueSuffix}${ext}`;
+
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: filename,
+            Body: Buffer.from(await profilePicture.arrayBuffer()),
+            ContentType: profilePicture.type,
+        };
+
+        const data = await s3.upload(params).promise();
+        profilePicturePath = data.Location;
+    } else {
+        profilePicturePath = "https://ui-avatars.com/api/?name=" + username + "&background=random&size=128";
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user_info = {
+        username,
+        password: hashedPassword,
+        email,
+        contactLink,
+        profilePicture: profilePicturePath,
+    };
+
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const { client, db } = await connectToDatabase();
+        const userCollection = db.collection("users");
+        const user = await userCollection.updateOne(
+            { _id: new ObjectId(decoded.id) },
+            { $set: user_info },
+        );
+
+        if (!user) {
+            return new Response(JSON.stringify({ user: null }), {
+                status: 404,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+
+        return new Response(JSON.stringify({ user }), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (error) {
+        console.log(error);
         return new Response(JSON.stringify({ user: null }), {
             status: 403,
             headers: {
